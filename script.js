f0d79fe2c30f1b5e1fc46601be90fd33648f7305
Torrex <flendik666@gmail.com>
2024-03-22 21:43:43 +0300

    add id changer

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 const adviceBtn = document.querySelector('.nextAdviceBtn');
 const advice = document.querySelector('.blockMainText');
+const adviceId = document.querySelector('.blockTitle');
 
 async function getRandomAdvice() {
   const response = await fetch('https://api.adviceslip.com/advice');
   const data = await response.json();
   
+  changeAdviceId(adviceId, data.slip.id)
+
   changeAdvice(advice, data.slip.advice);
+}
+
+function changeAdviceId (currentId, reqId) {
+  currentId.textContent = 'advice #' + reqId;
 }
 
 function changeAdvice (currentAdvice, reqAdvice) {
